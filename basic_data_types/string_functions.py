3f8439f89142bb40300695396e5d4624aba8225e
crazdrms <dilipmaharjann@gmail.com>
2017-12-06 20:54:29 -0500

    string and number functions

---
+++ basic_data_types/string_functions.py
@@ -0,0 +1,15 @@
+mystring='mystring'
+
+# changing case
+print(mystring.lower())
+print(mystring.upper())
+
+#repeatation
+print('*'*3)
+print(str(3))
+
+print('{} {} {}'.format('I','love','Python'))
+print('{0} {1} {2} {2} {1} {0}'.format('I','love','Python'))
+
+name=input("Write your name:")
+print(name.upper())
