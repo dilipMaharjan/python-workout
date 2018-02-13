8504ae566917f1af56208fc36566fe0537b08d28
crazdrms <dilipmaharjann@gmail.com>
2018-02-12 23:33:44 -0500

    uncommon codes

---
+++ uncommon/codes.py
@@ -0,0 +1,10 @@
+import keyword
+
+#list all the 33 keywords in python3
+print(keyword.kwlist)
+
+#gives data type of a
+#gives the address of a
+a=5
+print(type(a)) #int
+print(id(a)) #some address in memory
